2e6f57bbd35f4ed12848dc5b6cc117ae99bef93a
QPCA <qpca@outlook.com>
2017-01-18 00:24:03 +0700

    1st lab on Ruby

---
+++ lab1.rb
@@ -0,0 +1,37 @@
+require 'net/http'
+
+$site = "http://www.mosigra.ru/"
+$eArray = []
+$URLs = []
+$counter = 0
+
+def Parse(obj)
+	if ($counter < 10) then
+		$counter += 1
+		$URLs.push(obj)
+		
+		regExpE = /mailto:([\w.-]+@[\w.-]+\.\w+)/
+		regExpU = Regexp.new('<a href=[\'\"](' + $site + '[\w./]+)')
+		url = URI.parse(obj)
+		req = Net::HTTP::Get.new(url.to_s)
+		res = Net::HTTP.start(url.host, url.port) {|http|
+			http.request(req)}
+		page = res.body
+	
+		for e in page.scan(regExpE) 
+			unless ($eArray.include?(e.join(",")))
+				$eArray.push(e.join(","))
+			end
+		end
+		newURLs = page.scan(regExpU).uniq
+		for u in newURLs
+			unless ($URLs.include?(u.join("")))
+				Parse(u.join(""))
+			end
+		end
+	end
+end
+
+Parse($site)
+print($eArray)
+#print($URLs)
